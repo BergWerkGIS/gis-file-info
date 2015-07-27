afbbc131456dd76e1c3607fc2348a55901b9932b
bergwerkgis <wb@bergwerk-gis.at>
2015-07-27 21:27:22 +0200

    test usage of latest master, https://github.com/naturalatlas/node-gdal/commit/c1924b0ad0b147ef9cacde04c4720a4a5a14feac

--- detect.js
+++ detect.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var util = require('util');
 var gdal = require("gdal");
 
 if(process.argv.length<3){ return console.log('no tiff specified');}
@@ -7,24 +8,29 @@ var tiff_path=process.argv[2];
 if(!fs.existsSync(tiff_path)){ return console.log('not found: ' + tiff_path);}
 
 
-gdal.verbose();
-gdal.config.set('CPL_DEBUG', 'ON');
-gdal.config.set('CPL_LOG_ERRORS', 'ON');
-gdal.config.set('CPL_LOG', 'gdal-log.txt');
-//gdal.config.set('CPL_LOG_APPEND', 'TRUE');
+//gdal.verbose();
+//gdal.config.set('CPL_DEBUG', 'ON');
+//gdal.config.set('CPL_LOG_ERRORS', 'ON');
+//gdal.config.set('CPL_LOG', 'gdal-log.txt');
 
+//gdal.config.set('CPL_LOG_APPEND', 'TRUE');
 //hrm: not compiled for logging: Logging requires node-gdal be compiled with --enable_logging=true
 //gdal.startLogging({filename:'node-gdal-log.txt'});
 
 var dataset = gdal.open(tiff_path);
 var bnd_cnt = dataset.bands.count();
 
-for (var i = 0; i < bnd_cnt; i++) {
-	console.log('band ' + i + ' calculating check sum ...')
+for (var i = 1; i <= bnd_cnt; i++) {
+	console.log(util.format('band %d calculating check sum ...', i));
 	var check_sum = gdal.checksumImage(dataset.bands.get(i+1));
-	console.log('band ' + i + ' check sum: ', check_sum);
+	console.log(util.format('band %d check sum: %d', i, check_sum));
 	if (0 === check_sum) {
-		console.log('band ' + i + ' check sum is 0!!!\nexiting');
+		console.log(util.format('band %d check sum is 0!!!', i));
+		if (gdal.lastError) {
+			var le = gdal.lastError;
+			console.log(util.format('last error:\n  code  : %d\n  level : %d\n  msg   : %s', le.code, le.level, le.message));
+		};
+		console.log('exiting!');
 		break;
 	};
 };
