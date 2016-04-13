4323e94746c95015e3037bcafe3abf3b48ca66e1
bergwerkgis <wb@bergwerk-gis.at>
2016-04-13 16:55:38 -0700

    output NULL feature count

--- nodejs/layer-and-feature-count-via-node-gdal.js
+++ nodejs/layer-and-feature-count-via-node-gdal.js
@@ -5,20 +5,37 @@ var lyr_cnt = ds.layers.count();
 console.log('layers:', lyr_cnt);
 ds.layers.forEach(function (lyr) {
 	console.log(' -', lyr.name);
-	console.log(JSON.stringify(lyr));
+	//console.log(JSON.stringify(lyr));
 });
 var overall_feat_cnt = 0;
 ds.layers.forEach(function (lyr) {
 	console.log('------------------------', lyr.name, '-----------------------');
 	try {
 		var feat_cnt = 0;
+		var null_feat_cnt = 0;
 		lyr.features.forEach(function (feat) {
 			feat_cnt++;
+			var geom = feat.getGeometry();
+			if (!geom) {
+				null_feat_cnt++;
+				console.log(feat);
+			} else {
+				if (geom.isEmpty()) {
+					null_feat_cnt++;
+					console.log(feat);
+				}
+				if (gdal.wkbPoint == geom.wkbType) {
+
+				}
+			}
 		});
 		var lyr_feat_cnt = lyr.features.count();
 		console.log('lyr.features.count():', lyr_feat_cnt);
 		if (feat_cnt !== lyr_feat_cnt) {
 			console.error('feat_cnt !== lyr_feat_cnt', feat_cnt, lyr_feat_cnt);
+		}
+		if (null_feat_cnt > 0) {
+			console.log(null_feat_cnt, 'NULL features');
 		}
 		overall_feat_cnt += feat_cnt;
 	}
