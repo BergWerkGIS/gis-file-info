e3e0ebf8b3b00d28efc065393bd4f3d9468eae3b
bergwerkgis <wb@bergwerk-gis.at>
2016-06-03 07:57:16 +0000

    consider grouplayers (show feature count)

--- QGIS/show-feature-count-for-all-layers.py
+++ QGIS/show-feature-count-for-all-layers.py
@@ -1,8 +1,15 @@
+def show_feat_cnt(lyr):
+    #nodeType 1=layer, 0= group layer
+    if lyr.nodeType() ==  1:
+        lyr.setCustomProperty("showFeatureCount", True)
+    if lyr.nodeType() == 0:
+        for lyr2 in lyr.findLayers():
+            show_feat_cnt(lyr2)
+
+
 root = QgsProject.instance().layerTreeRoot()
 for lyr in root.findLayers():
     for prop in dir(lyr):
         #print prop
         pass
-    #nodeType 1=layer, 0= group layer
-    if lyr.nodeType() ==  1:
-        lyr.setCustomProperty("showFeatureCount", True)
+    show_feat_cnt(lyr)
