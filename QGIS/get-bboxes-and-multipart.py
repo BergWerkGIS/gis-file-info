f8fda634b75352948b42438b3e6702959f0710d8
bergwerkgis <wb@bergwerk-gis.at>
2016-06-29 10:35:56 +0000

    QGIS, get-bboxes: add fid or original features

--- QGIS/get-bboxes-and-multipart.py
+++ QGIS/get-bboxes-and-multipart.py
@@ -1,15 +1,32 @@
 verbose = False
+from PyQt4.QtCore import QVariant
 from qgis.gui import QgsRubberBand
 canvas = iface.mapCanvas()
 map_crs = canvas.mapRenderer().destinationCrs()
 map_authid = map_crs.authid()
 lyr = iface.activeLayer()
 lyr_authid = lyr.crs().authid()
+fld_orig_fid = 'origfid'
+
 lyr_pnt = QgsVectorLayer('Point?crs=' + lyr_authid + '&index=yes', 'points', 'memory')
+lyr_pnt.dataProvider().addAttributes([QgsField(fld_orig_fid, QVariant.Int)])
+lyr_pnt.updateFields()
+
 lyr_line = QgsVectorLayer('LineString?crs=' + lyr_authid + '&index=yes', 'lines', 'memory')
+lyr_line.dataProvider().addAttributes([QgsField(fld_orig_fid, QVariant.Int)])
+lyr_line.updateFields()
+
 lyr_polygon = QgsVectorLayer('Polygon?crs=' + lyr_authid + '&index=yes', 'poly', 'memory')
+lyr_polygon.dataProvider().addAttributes([QgsField(fld_orig_fid, QVariant.Int)])
+lyr_polygon.updateFields()
+
 lyr_bbox = QgsVectorLayer('Polygon?crs=' + lyr_authid + '&index=yes', 'bbox-features', 'memory')
+lyr_bbox.dataProvider().addAttributes([QgsField(fld_orig_fid, QVariant.Int)])
+lyr_bbox.updateFields()
+
 lyr_bbox_parts = QgsVectorLayer('Polygon?crs=' + lyr_authid + '&index=yes', 'bbox-parts', 'memory')
+lyr_bbox_parts.dataProvider().addAttributes([QgsField(fld_orig_fid, QVariant.Int)])
+lyr_bbox_parts.updateFields()
 
 #feats = lyr.getFeatures()
 feats = lyr.selectedFeatures() if lyr.selectedFeatureCount() > 0 else lyr.getFeatures()
@@ -27,8 +44,9 @@ for feat in feats:
     xmax = xmax if xmax > bb.xMaximum() else bb.xMaximum()
     ymax = ymax if ymax > bb.yMaximum() else bb.yMaximum()
 
-    feat_bb = QgsFeature()
+    feat_bb = QgsFeature(lyr_bbox.pendingFields())
     feat_bb.setGeometry(QgsGeometry.fromRect(bb))
+    feat_bb.setAttribute(fld_orig_fid, feat.id())
     lyr_bbox.dataProvider().addFeatures([feat_bb])
 
     geom = feat.geometry()
@@ -40,11 +58,18 @@ for feat in feats:
         parts = geom.asGeometryCollection()
         if verbose: print 'parts:', len(parts);
         for part in parts:
-            new_feat = QgsFeature()
+            if part.wkbType() == QGis.WKBPoint:
+                new_feat = QgsFeature(lyr_pnt.pendingFields())
+            elif part.wkbType() == QGis.WKBLineString:
+                new_feat = QgsFeature(lyr_line.pendingFields())
+            else:
+                new_feat = QgsFeature(lyr_polygon.pendingFields())
             new_feat.setGeometry(part)
+            new_feat.setAttribute(fld_orig_fid, feat.id())
             #print 'wkbType:', part.wkbType()
-            part_bb = QgsFeature()
+            part_bb = QgsFeature(lyr_bbox_parts.pendingFields())
             part_bb.setGeometry(QgsGeometry.fromRect(new_feat.geometry().boundingBox()))
+            part_bb.setAttribute(fld_orig_fid, feat.id())
             lyr_bbox_parts.dataProvider().addFeatures([part_bb])
             if part.wkbType() == QGis.WKBPoint:
                 #print 'point:', part.exportToWkt()
