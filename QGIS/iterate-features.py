9d35c70a291579b7b677a9d5a6b200da8ae318fa
BergWerkGIS <wb@BergWerk-GIS.at>
2017-05-29 13:08:16 +0200

    iterate features: more verbose output

--- QGIS/iterate-features.py
+++ QGIS/iterate-features.py
@@ -11,18 +11,20 @@ feat_cnt = 0
 feats = lyr.selectedFeatures() if lyr.selectedFeatureCount() > 0 else lyr.getFeatures()
 for feat in feats:
     feat_cnt+=1
-    print 'fid:', feat.id()
+    #print 'fid:', feat.id()
+    if feat_cnt % 1000 == 0:
+        print 'fid:', feat.id()
     if access_geom:
         geom = feat.geometry()
         #print geom.type()
         #print geom.boundingBox().toString()
         if not geom.isGeosValid ():
-          print 'NOT geos valid'
+          print feat.id(), 'NOT geos valid'
         #line = geom.asPolyline()
         #print len(line)
         geom_errors = geom.validateGeometry()
         if len(geom_errors) > 0:
-            print 'geometry NOT valid:'
+            print feat.id(), 'geometry NOT valid:'
             for geom_error in geom_errors:
                 print geom_error.what()
         
