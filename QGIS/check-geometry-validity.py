3879701cb4f7b7d74416080c180628eee9f55191
Wilhelm Berg <wb@BergWerk-GIS.at>
2019-03-25 15:28:20 +0100

    update to QGIS 3.x

--- QGIS/check-geometry-validity.py
+++ QGIS/check-geometry-validity.py
@@ -1,5 +1,5 @@
 import os
-from PyQt4.QtCore import QVariant
+from PyQt5.QtCore import QVariant
 
 lyr = iface.activeLayer()
 if lyr is None:
@@ -17,7 +17,7 @@ for in_feat in feats:
     feat_cnt+=1
     in_geom = in_feat.geometry()
     if in_geom is None:
-        print in_feat.id(), 'NULL geometry, skipped'
+        print(in_feat.id(), 'NULL geometry, skipped')
         skip_cnt += 1
         null_cnt += 1
         continue
@@ -29,39 +29,39 @@ for in_feat in feats:
     part_idx = 0
     for polygon in polygons:
         if len(polygon) > 100:
-            print 'fid:', in_feat.id(), 'rings:', len(polygon)
+            print('fid:', in_feat.id(), 'rings:', len(polygon))
         ring_idx = 0
         for ring in polygon:
             vertex_cnt = len(ring)
             overall_vertex_cnt += vertex_cnt
             if vertex_cnt > 3000:
-                print 'fid:', in_feat.id(), ' part:', part_idx, ' ring:', ring_idx, ' vertices:', vertex_cnt
+                print('fid:', in_feat.id(), ' part:', part_idx, ' ring:', ring_idx, ' vertices:', vertex_cnt)
 #            for vertex in ring:
 #                if vertex[0] <= -180 or vertex[0] >= 180 or vertex[1] <= -90 or vertex[1] >= 90:
-#                    print 'fid:', in_feat.id(), 'out of bounds:', vertex
+#                    print('fid:', in_feat.id(), 'out of bounds:', vertex)
             ring_idx += 1
         part_idx += 1
     if not in_geom.isGeosValid ():
-       print 'fid:', in_feat.id(), 'NOT geos valid'
+       print('fid:', in_feat.id(), 'NOT geos valid')
        geos_invalid_cnt += 1
        continue
     geom_errors = in_geom.validateGeometry()
     if len(geom_errors) > 0:
-        print 'fid:', in_feat.id(), 'geometry NOT valid:'
+        print('fid:', in_feat.id(), 'geometry NOT valid:')
         for geom_error in geom_errors:
-            print geom_error.what()
-        print 'fid:', in_feat.id(), 'skipped'
+            print(geom_error.what())
+        print('fid:', in_feat.id(), 'skipped')
         skip_cnt += 1
         qgis_invalid_cnt += 1
 
-print '-------'
-print 'layer feature count', lyr.featureCount()
-print 'dataprovider feature count', lyr.dataProvider().featureCount()
-print 'analyzed features', feat_cnt
-print 'multipart features', multipart_cnt
-print 'skipped:', skip_cnt
-print 'null geometries', null_cnt
-print 'qgis invalid:', qgis_invalid_cnt
-print 'geos invalid', geos_invalid_cnt
-print 'overall_vertex_cnt', overall_vertex_cnt
-print 'finished'
+print('-------')
+print('layer feature count', lyr.featureCount())
+print('dataprovider feature count', lyr.dataProvider().featureCount())
+print('analyzed features', feat_cnt)
+print('multipart features', multipart_cnt)
+print('skipped:', skip_cnt)
+print('null geometries', null_cnt)
+print('qgis invalid:', qgis_invalid_cnt)
+print('geos invalid', geos_invalid_cnt)
+print('overall_vertex_cnt', overall_vertex_cnt)
+print('finished')
--- QGIS/get-feature-and-multipart-count.py
+++ QGIS/get-feature-and-multipart-count.py
@@ -5,6 +5,7 @@ cnt_multi_part=0
 cnt_null_geom=0
 cnt_features_overall=0
 mp_feats = {}
+mp_vertices = {}
 for feat in feats:
     geom = feat.geometry()
     if geom is None:
@@ -16,41 +17,72 @@ for feat in feats:
         parts = geom.asGeometryCollection()
         cnt_features_overall += len(parts)
         mp_feats[feat.id()] = len(parts)
+        vertex_cnt = 0
+        parts = geom.asMultiPolygon()
+        for ring in parts:
+            for v in ring:
+                vertex_cnt += len(v)
+        mp_vertices[feat.id()] = vertex_cnt
     else:
         cnt_single_part += 1
         cnt_features_overall +=1
 
-print '{0} "{1}"'.format(lyr.crs().authid(), lyr.crs().description())
+print('{0} "{1}"'.format(lyr.crs().authid(), lyr.crs().description()))
 print
 
-print '* single part features:', cnt_single_part
-print '* multi part features:' , cnt_multi_part
-print '* null geometry features:' , cnt_null_geom
-print '* all features:', cnt_features_overall
-print ''
+print('* single part features:', cnt_single_part)
+print('* multi part features:' , cnt_multi_part)
+print('* null geometry features:' , cnt_null_geom)
+print('* sum of parts:', cnt_features_overall)
+print('')
 
 show_max = 10
 show_cnt = 1
-print '#### top', show_max, 'features with most multiparts'
-print ''
+print('#### top', show_max, 'features with most multiparts')
+print('')
 
 hdr = 'fid | parts'
 hdr2 = '----- | -----'
-for fld in lyr.pendingFields():
+for fld in lyr.fields():
     hdr += ' | ' + fld.name()
     hdr2 += ' | -----'
-print hdr
-print hdr2
+print(hdr)
+print(hdr2)
 for key, val in sorted(mp_feats.items(), key=lambda kv: kv[1], reverse=True):
     req = QgsFeatureRequest(key)
-    f = lyr.getFeatures(req).next()
+    f = next(lyr.getFeatures(req))
     r = u'{0} | **{1}**'.format(key, val)
     for a in f.attributes():
         r += u' | {0}'.format(a)
-    print r
+    print(r)
 
     if show_cnt >= show_max:
         break
     show_cnt += 1
 
-print 'finished'
+
+print('')
+show_cnt = 1
+print('#### top', show_max, 'features with most vertices')
+
+print('')
+hdr = 'fid | vertices'
+hdr2 = '----- | -----'
+for fld in lyr.fields():
+    hdr += ' | ' + fld.name()
+    hdr2 += ' | -----'
+print(hdr)
+print(hdr2)
+for key, val in sorted(mp_vertices.items(), key=lambda kv: kv[1], reverse=True):
+    req = QgsFeatureRequest(key)
+    f = next(lyr.getFeatures(req))
+    r = u'{0} | **{1}**'.format(key, val)
+    for a in f.attributes():
+        r += u' | {0}'.format(a)
+    print(r)
+
+    if show_cnt >= show_max:
+        break
+    show_cnt += 1
+
+print('finished')
