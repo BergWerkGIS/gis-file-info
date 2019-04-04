393f7adf42279b6191fb67aaae29527ce4b5c9ae
Wilhelm Berg <wb@BergWerk-GIS.at>
2019-04-04 09:47:19 -0400

    update to 3.x

--- QGIS/iterate-features.py
+++ QGIS/iterate-features.py
@@ -1,7 +1,7 @@
 access_geom = True
 
 import os
-from PyQt4.QtCore import QVariant
+from PyQt5.QtCore import QVariant
 
 lyr = iface.activeLayer()
 if lyr is None:
@@ -11,25 +11,25 @@ feat_cnt = 0
 feats = lyr.selectedFeatures() if lyr.selectedFeatureCount() > 0 else lyr.getFeatures()
 for feat in feats:
     feat_cnt+=1
-    #print 'fid:', feat.id()
+    #print('fid:', feat.id())
     if feat_cnt % 1000 == 0:
-        print 'fid:', feat.id()
+        print('fid:', feat.id())
     if access_geom:
         geom = feat.geometry()
-        #print geom.type()
-        #print geom.boundingBox().toString()
+        #print(geom.type())
+        #print(geom.boundingBox().toString())
         if not geom.isGeosValid ():
-          print feat.id(), 'NOT geos valid'
+          print(feat.id(), 'NOT geos valid')
         #line = geom.asPolyline()
-        #print len(line)
+        #print(len(line))
         geom_errors = geom.validateGeometry()
         if len(geom_errors) > 0:
-            print feat.id(), 'geometry NOT valid:'
+            print(feat.id(), 'geometry NOT valid:')
             for geom_error in geom_errors:
-                print geom_error.what()
+                print(geom_error.what())
         
-print '-------'
-print 'iterated features', feat_cnt
-print 'layer feature count', lyr.featureCount()
-print 'dataprovider feature count', lyr.dataProvider().featureCount()
-print 'finished'
+print('-------')
+print('iterated features', feat_cnt)
+print('layer feature count', lyr.featureCount())
+print('dataprovider feature count', lyr.dataProvider().featureCount())
+print('finished')
